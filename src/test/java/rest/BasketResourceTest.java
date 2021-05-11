017f98ddac796b5d944bdbecf8a6072f864a336e
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-05-11 13:00:58 +0200

    feat: get users active basket endpoint

--- src/test/java/rest/BasketResourceTest.java
+++ src/test/java/rest/BasketResourceTest.java
@@ -10,13 +10,19 @@ import dtos.BasketItemDTO;
 
 import static io.restassured.RestAssured.given;
 
+import entities.Role;
+import entities.User;
+import entities.basket.Basket;
+import entities.basket.BasketItem;
 import io.restassured.http.ContentType;
+import javax.persistence.EntityManager;
 import javax.ws.rs.core.Response;
 import org.glassfish.grizzly.http.util.HttpStatus;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,6 +33,8 @@ import static rest.SetupRestTests.shutdownServer;
  * @author peter
  */
 public class BasketResourceTest extends SetupRestTests {
+
+    Basket b1;
 
     public BasketResourceTest() {
     }
@@ -41,17 +49,89 @@ public class BasketResourceTest extends SetupRestTests {
         shutdownServer();
     }
 
+    @BeforeEach
+    void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            User user = new User("user", "1234");
+            Role role = new Role("user");
+
+            em.getTransaction().begin();
+            em.persist(role);
+            em.persist(user);
+            user.addRole(role);
+
+            b1 = new Basket(user);
+            b1.addItems(new BasketItem("Sushi", 2, 10, 9.99));
+            em.persist(b1);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createQuery("DELETE FROM BasketItem").executeUpdate();
+            em.createQuery("DELETE FROM Basket").executeUpdate();
+            em.createQuery("DELETE FROM Role").executeUpdate();
+            em.createNamedQuery("User.deleteAllRows").executeUpdate();
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    private String login(String role, String password) {
+        String json = String.format("{username: \"%s\", password: \"%s\"}", role, password);
+        return given()
+            .contentType("application/json")
+            .body(json)
+            .when().post("/login")
+            .then()
+            .extract().path("token");
+    }
+
     @Test
     public void testAddItem() {
+        String token = login("user", "1234");
         BasketItemDTO requestBody = new BasketItemDTO("Sushi", 1, 2, 3.0);
         given()
             .contentType(ContentType.JSON)
+            .header("x-access-token", token)
             .body(requestBody)
             .when()
             .post("/basket/add/user")
             .then()
             .assertThat()
             .statusCode(HttpStatus.OK_200.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("get users active basket returns a basket")
+    void getUsersActiveBasketReturnsABasket() {
+        String token = login("user", "1234");
+        given()
+            .contentType(ContentType.JSON)
+            .header("x-access-token", token)
+            .when()
+            .get("/basket/active")
+            .then()
+            .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("get users active basket throws error if no user")
+    void getUsersActiveBasketThrowsErrorIfNoUser() {
+        given()
+            .contentType(ContentType.JSON)
+            .when()
+            .get("/basket/active")
+            .then()
+            .statusCode(403);
     }
 
 }
